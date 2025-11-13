da3523ca3fce78394b80566110af92a52af7ec91
Kaitlin <cookykn@gmail.com>
2025-11-12 22:41:10 -0800

    blah

--- Assets/OpenCV+Unity/Demo/Scripts/WebCamera.cs
+++ Assets/OpenCV+Unity/Demo/Scripts/WebCamera.cs
@@ -105,11 +105,70 @@ namespace OpenCvSharp.Demo
 			//UnityEngine.Debug.Log (string.Format("front = {0}, vertMirrored = {1}, angle = {2}", webCamDevice.isFrontFacing, webCamTexture.videoVerticallyMirrored, webCamTexture.videoRotationAngle));
 		}
 
+#if UNITY_IOS || UNITY_WEBGL
+		private bool CheckPermissionAndRaiseCallbackIfGranted(UserAuthorization authenticationType, Action authenticationGrantedAction)
+		{
+			if (Application.HasUserAuthorization(authenticationType))
+			{
+				if (authenticationGrantedAction != null)
+					authenticationGrantedAction();
+
+				return true;
+			}
+			return false;
+		}
+
+		private IEnumerator AskForPermissionIfRequired(UserAuthorization authenticationType, Action authenticationGrantedAction)
+		{
+			if (!CheckPermissionAndRaiseCallbackIfGranted(authenticationType, authenticationGrantedAction))
+			{
+				yield return Application.RequestUserAuthorization(authenticationType);
+				if (!CheckPermissionAndRaiseCallbackIfGranted(authenticationType, authenticationGrantedAction))
+					Debug.LogWarning($"Permission {authenticationType} Denied");
+			}
+		}
+#elif UNITY_ANDROID
+		private void PermissionCallbacksPermissionGranted(string permissionName)
+		{
+			StartCoroutine(DelayedCameraInitialization());
+		}
+
+		private IEnumerator DelayedCameraInitialization()
+		{
+			yield return null;
+			InitializeCamera();
+		}
+
+		private void PermissionCallbacksPermissionDenied(string permissionName)
+		{
+			Debug.LogWarning($"Permission {permissionName} Denied");
+		}
+
+		private void AskCameraPermission()
+		{
+			var callbacks = new PermissionCallbacks();
+			callbacks.PermissionDenied += PermissionCallbacksPermissionDenied;
+			callbacks.PermissionGranted += PermissionCallbacksPermissionGranted;
+			Permission.RequestUserPermission(Permission.Camera, callbacks);
+		}
+#endif
+
 		/// <summary>
 		/// Default initializer for MonoBehavior sub-classes
 		/// </summary>
 		protected virtual void Awake()
 		{
+#if UNITY_IOS || UNITY_WEBGL
+			StartCoroutine(AskForPermissionIfRequired(UserAuthorization.WebCam, () => { InitializeCamera(); }));
+			return;
+#elif UNITY_ANDROID
+			if (!Permission.HasUserAuthorizedPermission(Permission.Camera))
+			{
+				AskCameraPermission();
+				return;
+			}
+#endif
+			//InitializeCamera();
 			if (WebCamTexture.devices.Length > 0)
 				DeviceName = WebCamTexture.devices[WebCamTexture.devices.Length - 1].name;
 		}
